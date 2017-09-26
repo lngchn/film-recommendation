b7be82c684729c46d24d2bc449f4f70bf6409a78
Liang Chen <lngchn85@gmail.com>
2017-09-26 16:00:40 -0400

    Add 'Add a Film' button to navbar

--- film-recommender/src/component/navbar.js
+++ film-recommender/src/component/navbar.js
@@ -52,11 +52,20 @@ class Navbar extends React.Component {
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-            <div className="navbar-nav">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <div className="navbar-nav mr-auto">
               <NavItem name="Recommendation" active={this.state.active} onClick={value => this.handleClick("Recommendation")} />
               <NavItem name="Activity" active={this.state.active} onClick={value => this.handleClick("Activity")} />
               <NavItem name="People" active={this.state.active} onClick={value => this.handleClick("People")} />
+            </div>
+            <div className="btn-group mr-4" role="group">
+              <button id="btnGroupDrop1" type="button" className="btn btn-outline-info dropdown-toggle text-uppercase" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Add a Film
+              </button>
+              <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                <a className="dropdown-item" href="#">Dropdown link</a>
+                <a className="dropdown-item" href="#">Dropdown link</a>
+              </div>
             </div>
             <form className="form-inline" onSubmit={this.handleSubmit}>
               <input className="form-control mr-sm-2" type="text" value={this.state.searchValue} onChange={this.handleChange} placeholder="Search" aria-label="Search" />
