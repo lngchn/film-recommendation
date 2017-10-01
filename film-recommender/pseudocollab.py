d749dbae01bc97a5c02e9f3ade0bf1406968f027
Dan Cambitsis <dcam90@gmail.com>
2017-10-01 12:37:54 -0400

    Update pseudocollab.py

--- film-recommender/pseudocollab.py
+++ film-recommender/pseudocollab.py
@@ -1,64 +1,65 @@
-'''TO DO: need a dataset of json files acquired from TMDB/IMDB
-1) 5,000 users+ from IMDB, 10,000+ films from IMDB
-2) schema of database --> random IMDB users with ratings of films
-^^^ use Kwabena's code?
-3)  euclidean vs. manhattan vs. pearson correlation (will probably use this)
-3a) euclidean between two points: square root of summation of (x1 - y1)^2
-    --> better than manhattan but still too basic
-3b) manhattan is the absolute value of the summation of x1 - y1 --> too basic
-3c) pearson's --> calculate r-value, complicated formula that I will need to implement
-3d) hamming distance? --> may not be relevant
-3e) jaccard distance? --> may look into this, may be way too complicated but seems most precise
+ ̶'̶'̶'̶T̶O̶ ̶D̶O̶:̶ ̶n̶e̶e̶d̶ ̶a̶ ̶d̶a̶t̶a̶s̶e̶t̶ ̶o̶f̶ ̶j̶s̶o̶n̶ ̶f̶i̶l̶e̶s̶ ̶a̶c̶q̶u̶i̶r̶e̶d̶ ̶f̶r̶o̶m̶ ̶T̶M̶D̶B̶/̶I̶M̶D̶B̶
+̶1̶)̶ ̶5̶,̶0̶0̶0̶ ̶u̶s̶e̶r̶s̶+̶ ̶f̶r̶o̶m̶ ̶I̶M̶D̶B̶,̶ ̶1̶0̶,̶0̶0̶0̶+̶ ̶f̶i̶l̶m̶s̶ ̶f̶r̶o̶m̶ ̶I̶M̶D̶B̶
+̶2̶)̶ ̶s̶c̶h̶e̶m̶a̶ ̶o̶f̶ ̶d̶a̶t̶a̶b̶a̶s̶e̶ ̶-̶-̶>̶ ̶r̶a̶n̶d̶o̶m̶ ̶I̶M̶D̶B̶ ̶u̶s̶e̶r̶s̶ ̶w̶i̶t̶h̶ ̶r̶a̶t̶i̶n̶g̶s̶ ̶o̶f̶ ̶f̶i̶l̶m̶s̶
+̶^̶^̶^̶ ̶u̶s̶e̶ ̶K̶w̶a̶b̶e̶n̶a̶'̶s̶ ̶c̶o̶d̶e̶?̶
+̶3̶)̶ ̶ ̶e̶u̶c̶l̶i̶d̶e̶a̶n̶ ̶v̶s̶.̶ ̶m̶a̶n̶h̶a̶t̶t̶a̶n̶ ̶v̶s̶.̶ ̶p̶e̶a̶r̶s̶o̶n̶ ̶c̶o̶r̶r̶e̶l̶a̶t̶i̶o̶n̶ ̶(̶w̶i̶l̶l̶ ̶p̶r̶o̶b̶a̶b̶l̶y̶ ̶u̶s̶e̶ ̶t̶h̶i̶s̶)̶
+̶3̶a̶)̶ ̶e̶u̶c̶l̶i̶d̶e̶a̶n̶ ̶b̶e̶t̶w̶e̶e̶n̶ ̶t̶w̶o̶ ̶p̶o̶i̶n̶t̶s̶:̶ ̶s̶q̶u̶a̶r̶e̶ ̶r̶o̶o̶t̶ ̶o̶f̶ ̶s̶u̶m̶m̶a̶t̶i̶o̶n̶ ̶o̶f̶ ̶(̶x̶1̶ ̶-̶ ̶y̶1̶)̶^̶2̶
+̶ ̶ ̶ ̶ ̶-̶-̶>̶ ̶b̶e̶t̶t̶e̶r̶ ̶t̶h̶a̶n̶ ̶m̶a̶n̶h̶a̶t̶t̶a̶n̶ ̶b̶u̶t̶ ̶s̶t̶i̶l̶l̶ ̶t̶o̶o̶ ̶b̶a̶s̶i̶c̶
+̶3̶b̶)̶ ̶m̶a̶n̶h̶a̶t̶t̶a̶n̶ ̶i̶s̶ ̶t̶h̶e̶ ̶a̶b̶s̶o̶l̶u̶t̶e̶ ̶v̶a̶l̶u̶e̶ ̶o̶f̶ ̶t̶h̶e̶ ̶s̶u̶m̶m̶a̶t̶i̶o̶n̶ ̶o̶f̶ ̶x̶1̶ ̶-̶ ̶y̶1̶ ̶-̶-̶>̶ ̶t̶o̶o̶ ̶b̶a̶s̶i̶c̶
+̶3̶c̶)̶ ̶p̶e̶a̶r̶s̶o̶n̶'̶s̶ ̶-̶-̶>̶ ̶c̶a̶l̶c̶u̶l̶a̶t̶e̶ ̶r̶-̶v̶a̶l̶u̶e̶,̶ ̶c̶o̶m̶p̶l̶i̶c̶a̶t̶e̶d̶ ̶f̶o̶r̶m̶u̶l̶a̶ ̶t̶h̶a̶t̶ ̶I̶ ̶w̶i̶l̶l̶ ̶n̶e̶e̶d̶ ̶t̶o̶ ̶i̶m̶p̶l̶e̶m̶e̶n̶t̶
+̶3̶d̶)̶ ̶h̶a̶m̶m̶i̶n̶g̶ ̶d̶i̶s̶t̶a̶n̶c̶e̶?̶ ̶-̶-̶>̶ ̶m̶a̶y̶ ̶n̶o̶t̶ ̶b̶e̶ ̶r̶e̶l̶e̶v̶a̶n̶t̶
+̶3̶e̶)̶ ̶j̶a̶c̶c̶a̶r̶d̶ ̶d̶i̶s̶t̶a̶n̶c̶e̶?̶ ̶-̶-̶>̶ ̶m̶a̶y̶ ̶l̶o̶o̶k̶ ̶i̶n̶t̶o̶ ̶t̶h̶i̶s̶,̶ ̶m̶a̶y̶ ̶b̶e̶ ̶w̶a̶y̶ ̶t̶o̶o̶ ̶c̶o̶m̶p̶l̶i̶c̶a̶t̶e̶d̶ ̶b̶u̶t̶ ̶s̶e̶e̶m̶s̶ ̶m̶o̶s̶t̶ ̶p̶r̶e̶c̶i̶s̶e̶
+̶'̶'̶'̶
+̶
+̶'̶'̶'̶T̶O̶ ̶D̶O̶:̶ ̶c̶a̶l̶c̶u̶l̶a̶t̶e̶ ̶s̶i̶m̶i̶l̶a̶r̶i̶t̶y̶ ̶"̶s̶c̶o̶r̶e̶"̶ ̶b̶e̶t̶w̶e̶e̶n̶ ̶m̶o̶v̶i̶e̶s̶ ̶u̶s̶i̶n̶g̶ ̶p̶e̶a̶r̶s̶o̶n̶'̶s̶ ̶c̶o̶r̶r̶e̶l̶a̶t̶i̶o̶n̶ ̶(̶b̶e̶s̶t̶ ̶c̶h̶o̶i̶c̶e̶ ̶t̶h̶u̶s̶ ̶f̶a̶r̶)̶
+̶p̶a̶r̶a̶m̶e̶t̶e̶r̶s̶:̶ ̶t̶w̶o̶ ̶p̶e̶o̶p̶l̶e̶
+̶p̶1̶ ̶-̶>̶ ̶p̶e̶r̶s̶o̶n̶1̶,̶ ̶t̶h̶e̶ ̶p̶e̶r̶s̶o̶n̶ ̶c̶u̶r̶r̶e̶n̶t̶l̶y̶ ̶u̶s̶i̶n̶g̶ ̶t̶h̶e̶ ̶w̶e̶b̶s̶i̶t̶e̶
+̶p̶2̶ ̶-̶>̶ ̶p̶e̶r̶s̶o̶n̶2̶,̶ ̶p̶e̶o̶p̶l̶e̶ ̶i̶n̶ ̶t̶h̶e̶ ̶d̶a̶t̶a̶b̶a̶s̶e̶ ̶(̶w̶e̶ ̶w̶i̶l̶l̶ ̶c̶o̶m̶p̶a̶r̶e̶ ̶p̶e̶r̶s̶o̶n̶1̶ ̶t̶o̶ ̶e̶v̶e̶r̶y̶o̶n̶e̶ ̶i̶n̶ ̶t̶h̶e̶
+̶d̶a̶t̶a̶b̶a̶s̶e̶
+̶'̶'̶'̶
+̶d̶e̶f̶ ̶s̶_̶s̶c̶o̶r̶e̶(̶p̶1̶,̶ ̶p̶2̶)̶:̶
+̶ ̶ ̶ ̶ ̶#̶#̶T̶O̶ ̶D̶O̶:̶ ̶w̶e̶ ̶s̶h̶o̶u̶l̶d̶ ̶c̶h̶e̶c̶k̶ ̶t̶o̶ ̶s̶e̶e̶ ̶i̶f̶ ̶p̶e̶r̶s̶o̶n̶1̶ ̶a̶n̶d̶ ̶p̶e̶r̶s̶o̶n̶2̶ ̶h̶a̶v̶e̶ ̶a̶n̶y̶ ̶c̶o̶m̶m̶o̶n̶ ̶f̶i̶l̶m̶s̶
+̶ ̶ ̶ ̶ ̶#̶#̶i̶f̶ ̶t̶h̶e̶y̶ ̶d̶o̶n̶'̶t̶,̶ ̶b̶r̶e̶a̶k̶ ̶t̶h̶e̶ ̶f̶u̶n̶c̶t̶i̶o̶n̶ ̶(̶r̶e̶t̶u̶r̶n̶ ̶0̶)̶
+̶ ̶ ̶ ̶ ̶#̶#̶i̶f̶ ̶t̶h̶e̶y̶ ̶d̶o̶:̶
+̶ ̶ ̶ ̶ ̶ ̶ ̶ ̶ ̶#̶#̶ ̶c̶a̶l̶c̶u̶l̶a̶t̶e̶ ̶t̶h̶e̶ ̶p̶e̶a̶r̶s̶o̶n̶'̶s̶ ̶c̶o̶r̶r̶e̶l̶a̶t̶i̶o̶n̶ ̶b̶e̶t̶w̶e̶e̶n̶ ̶t̶h̶e̶ ̶t̶w̶o̶ ̶p̶e̶o̶p̶l̶e̶
+̶
+̶'̶'̶'̶T̶O̶ ̶D̶O̶:̶ ̶c̶r̶e̶a̶t̶e̶ ̶a̶ ̶f̶u̶n̶c̶t̶i̶o̶n̶ ̶t̶h̶a̶t̶ ̶d̶i̶s̶p̶l̶a̶y̶s̶ ̶s̶i̶m̶i̶l̶a̶r̶ ̶u̶s̶e̶r̶s̶ ̶t̶o̶ ̶p̶e̶r̶s̶o̶n̶1̶
+̶p̶a̶r̶a̶m̶e̶t̶e̶r̶s̶:̶ ̶p̶e̶r̶s̶o̶n̶1̶ ̶(̶y̶o̶u̶ ̶o̶r̶ ̶w̶h̶o̶e̶v̶e̶r̶ ̶i̶s̶ ̶u̶s̶i̶n̶g̶ ̶t̶h̶e̶ ̶w̶e̶b̶s̶i̶t̶e̶)̶,̶ ̶#̶ ̶o̶f̶ ̶u̶s̶e̶r̶s̶ ̶i̶n̶ ̶t̶h̶e̶ ̶d̶a̶t̶a̶b̶a̶s̶e̶
+̶'̶'̶'̶
+̶
+̶d̶e̶f̶ ̶s̶_̶u̶s̶e̶r̶s̶(̶p̶1̶,̶ ̶d̶a̶t̶a̶b̶a̶s̶e̶_̶p̶p̶l̶)̶:̶
+̶ ̶ ̶ ̶ ̶#̶#̶T̶O̶ ̶D̶O̶:̶ ̶h̶a̶v̶e̶ ̶t̶h̶e̶ ̶m̶o̶s̶t̶ ̶s̶i̶m̶i̶l̶a̶r̶ ̶u̶s̶e̶r̶s̶ ̶t̶o̶ ̶p̶e̶r̶s̶o̶n̶1̶ ̶"̶b̶u̶b̶b̶l̶e̶ ̶u̶p̶"̶ ̶t̶o̶ ̶t̶h̶e̶ ̶t̶o̶p̶ ̶o̶f̶ ̶a̶ ̶l̶i̶s̶t̶/̶d̶a̶t̶a̶b̶a̶s̶e̶?̶
+̶ ̶ ̶ ̶ ̶ ̶ ̶ ̶ ̶#̶#̶w̶i̶l̶l̶ ̶n̶e̶e̶d̶ ̶t̶o̶ ̶t̶h̶i̶n̶k̶ ̶h̶o̶w̶ ̶t̶o̶ ̶d̶o̶ ̶t̶h̶i̶s̶
+̶ ̶ ̶ ̶ ̶#̶#̶s̶h̶o̶u̶l̶d̶,̶ ̶i̶n̶ ̶t̶h̶e̶ ̶e̶n̶d̶,̶ ̶r̶e̶t̶u̶r̶n̶ ̶s̶i̶m̶i̶l̶a̶r̶ ̶u̶s̶e̶r̶s̶ ̶t̶o̶ ̶p̶e̶r̶s̶o̶n̶1̶,̶ ̶d̶i̶s̶r̶e̶g̶a̶r̶d̶ ̶t̶h̶o̶s̶e̶ ̶w̶h̶o̶ ̶a̶r̶e̶n̶'̶t̶ ̶s̶i̶m̶i̶l̶a̶r̶
+̶ ̶ ̶ ̶ ̶ ̶ ̶ ̶ ̶#̶#̶c̶u̶t̶o̶f̶f̶?̶
+̶
+̶'̶'̶'̶T̶O̶ ̶D̶O̶:̶ ̶t̶h̶e̶ ̶m̶a̶i̶n̶ ̶f̶u̶n̶c̶t̶i̶o̶n̶,̶ ̶w̶h̶e̶r̶e̶ ̶t̶h̶e̶ ̶m̶a̶g̶i̶c̶ ̶h̶a̶p̶p̶e̶n̶s̶ ̶a̶k̶a̶ ̶s̶p̶i̶t̶s̶ ̶o̶u̶t̶ ̶t̶h̶e̶ ̶r̶e̶c̶o̶m̶m̶e̶n̶d̶e̶d̶ ̶m̶o̶v̶i̶e̶s̶
+̶p̶a̶r̶a̶m̶e̶t̶e̶r̶:̶ ̶y̶o̶u̶,̶ ̶o̶r̶ ̶p̶e̶r̶s̶o̶n̶1̶
+̶'̶'̶'̶
+̶
+̶d̶e̶f̶ ̶c̶o̶l̶a̶b̶_̶f̶i̶l̶t̶(̶p̶1̶)̶:̶
+̶ ̶ ̶ ̶ ̶#̶#̶T̶O̶ ̶D̶O̶:̶ ̶w̶e̶ ̶s̶h̶o̶u̶l̶d̶ ̶i̶g̶n̶o̶r̶e̶:̶ ̶i̶n̶v̶a̶l̶i̶d̶ ̶s̶c̶o̶r̶e̶s̶ ̶a̶n̶d̶ ̶m̶o̶v̶i̶e̶s̶ ̶p̶1̶ ̶h̶a̶s̶n̶'̶t̶ ̶s̶e̶e̶n̶
+̶ ̶ ̶ ̶ ̶#̶#̶c̶a̶l̶c̶u̶l̶a̶t̶e̶ ̶t̶h̶e̶ ̶s̶i̶m̶i̶l̶a̶r̶i̶t̶y̶ ̶s̶c̶o̶r̶e̶s̶ ̶t̶o̶ ̶e̶v̶e̶r̶y̶o̶n̶e̶ ̶i̶n̶ ̶t̶h̶e̶ ̶d̶a̶t̶a̶b̶a̶s̶e̶
+̶ ̶ ̶ ̶ ̶#̶#̶g̶e̶t̶ ̶t̶h̶e̶ ̶m̶o̶v̶i̶e̶s̶ ̶o̶f̶ ̶t̶h̶o̶s̶e̶ ̶m̶o̶s̶t̶ ̶s̶i̶m̶i̶l̶a̶r̶ ̶t̶o̶ ̶p̶1̶'̶s̶ ̶p̶r̶e̶f̶e̶r̶e̶n̶c̶e̶s̶
+̶ ̶ ̶ ̶ ̶#̶#̶d̶i̶s̶p̶l̶a̶y̶ ̶t̶h̶e̶m̶ ̶(̶w̶i̶l̶l̶ ̶m̶o̶s̶t̶ ̶l̶i̶k̶e̶l̶y̶ ̶s̶t̶o̶r̶e̶ ̶t̶h̶e̶ ̶m̶o̶v̶i̶e̶s̶ ̶i̶n̶ ̶a̶ ̶l̶i̶s̶t̶/̶a̶r̶r̶a̶y̶/̶w̶h̶a̶t̶e̶v̶e̶r̶,̶ ̶f̶r̶o̶n̶t̶ ̶e̶n̶d̶ ̶s̶h̶o̶u̶l̶d̶ ̶t̶a̶k̶e̶ ̶c̶a̶r̶e̶
+̶ ̶ ̶ ̶ ̶#̶#̶o̶f̶ ̶d̶i̶s̶p̶l̶a̶y̶i̶n̶g̶ ̶p̶o̶s̶t̶e̶r̶s̶ ̶a̶n̶d̶ ̶w̶h̶a̶t̶n̶o̶t̶
+̶
+̶'̶'̶'̶ ̶d̶r̶a̶w̶b̶a̶c̶k̶:̶ ̶a̶b̶o̶v̶e̶ ̶m̶a̶y̶ ̶b̶e̶ ̶t̶o̶o̶ ̶s̶l̶o̶w̶,̶ ̶m̶a̶y̶ ̶h̶a̶v̶e̶ ̶t̶o̶ ̶s̶e̶e̶ ̶w̶h̶a̶t̶ ̶h̶a̶p̶p̶e̶n̶s̶ ̶d̶u̶r̶i̶n̶g̶ ̶a̶c̶t̶u̶a̶l̶ ̶i̶m̶p̶l̶e̶m̶e̶n̶t̶a̶i̶o̶n̶.̶ ̶a̶n̶o̶t̶h̶e̶r̶ ̶p̶o̶s̶s̶i̶b̶i̶l̶i̶t̶y̶:̶'̶'̶'̶
+
 '''
-
-'''TO DO: calculate similarity "score" between movies using pearson's correlation (best choice thus far)
-parameters: two people
-p1 -> person1, the person currently using the website
-p2 -> person2, people in the database (we will compare person1 to everyone in the
-database
+Gameplan:
+1) read from database directly OR create a temporary database and work from that (?)
+2) get only the users from the database/temp database where ratings of SAME movies are higher, match, or close to rating
+    i.e. user 1 from IMDB rated Goldeneye a 9.0, user on our website rated it 10, we should match users
+    user 6 from IMDB rated Goldeneye a 8.5, we should match with user on our website (close enough)
+    user 13 from IMDB rated Goldeneye a 7.4, we should ignore user entirely
+3) we should have those with most matches on top and descend (sort)
+4) extract the IDs of the IMDB users 
+5) get all of the movies from each ID 
+6) remove all duplicate movies
+7) remove all users who have less than a certain # of ratings
+8) establish a minimum rating (>7 ?) and get movies where ID users rated films above that rating
 '''
-def s_score(p1, p2):
-    ##TO DO: we should check to see if person1 and person2 have any common films
-    ##if they don't, break the function (return 0)
-    ##if they do:
-        ## calculate the pearson's correlation between the two people
-
-'''TO DO: create a function that displays similar users to person1
-parameters: person1 (you or whoever is using the website), # of users in the database
-'''
-
-def s_users(p1, database_ppl):
-    ##TO DO: have the most similar users to person1 "bubble up" to the top of a list/database?
-        ##will need to think how to do this
-    ##should, in the end, return similar users to person1, disregard those who aren't similar
-        ##cutoff?
-
-'''TO DO: the main function, where the magic happens aka spits out the recommended movies
-parameter: you, or person1
-'''
-
-def colab_filt(p1):
-    ##TO DO: we should ignore: invalid scores and movies p1 hasn't seen
-    ##calculate the similarity scores to everyone in the database
-    ##get the movies of those most similar to p1's preferences
-    ##display them (will most likely store the movies in a list/array/whatever, front end should take care
-    ##of displaying posters and whatnot
-
-''' drawback: above may be too slow, may have to see what happens during actual implementaion. another possibility:'''
-
-###############
-
-'''an approach that does not require too many loops (the above might use a lot of for/while loops hence the slowness) and utilizes
-both user-based AND item-based filtering
-1) user-based --> similarities between user history
-2) item-based --> similarities between movie history
-
-Rather than calculating distance, I would use cosine similarities (from python library scipy)
-1) I'd first apply item-based filtering
-1a) i.e. -> match Toy Story similar films such as Toy Story 2, Bug's Life, Up, etc. (animation films)
-2) THEN I would apply user-based filtering
-
-^^^ above is a work in progress, I will need to do more research'''
 
 
 
