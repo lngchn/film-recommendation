d9257004253097904a72296218a5d3ade1131ab9
emurzame <emilmurzametov@yahoo.com>
2017-10-03 18:29:29 -0400

    update to server.js

--- film-recommender/scripts/server.js
+++ film-recommender/scripts/server.js
@@ -12,17 +12,3 @@ app.get('/', function (req, res) {
 app.listen( app.get('port'), function () {
     console.log('Server running at http://localhost:%s', app.get('port'));
 });
-
-/*
-app.use( express.static(
-    path.join(__dirname, '../', 'public'),
-    {
-        //index
-        extensions:['html']
-    })
-);
-
-app.get( '/*', (req, res) => {
-    res.sendFile( path.join(__dirname, '../', 'public', '200.html') );
-});
-*/
