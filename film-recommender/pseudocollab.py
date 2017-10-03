a877bb0c0c62a0854c3a3c265ffa9ace994d71d4
Dan Cambitsis <dcam90@gmail.com>
2017-10-03 15:15:11 -0400

    Update pseudocollab.py

--- film-recommender/pseudocollab.py
+++ film-recommender/pseudocollab.py
@@ -19,7 +19,37 @@ def pearson(p1, p2):
     ##TO DO: check to see if person1 and person2 have common films
     ##no common films = break, otherwise calculate the pearson's correlation between the two people
     ##WARNING: may not scale, may need to find another way to do this
-
+    ###############
+    
+    common_films = {} ##dictionary, key: movie itself, value: either 1 or 0
+    
+    ##here, implement a for loop to search for common preferences within the database
+    for movie in USER_USING_OUR_WEBSITE_MOVIE_PREFERENCES:
+        if movie in ROW_OF_DATABASE:
+            common_films[movie] = 1 ##it exists, set its value to 1
+        else:
+            return 0 ##terminate, we found a film that isn't shared
+        
+    ##begin the pearson (we have to: a) add up the preferences, b) sum up the squares, c) sum
+    ##up the products and lastly, d) do pearson's correlation from a, b, and c
+    
+    ##add up the preferences
+    p1_sum = sum(USER_USING_OUR_WEBSITE_MOVIE_PREFERENCES[movie] for movie in common_films)
+    p2_sum = sum(ROW_OF_DATABASE_THAT_MATCHES_USER[movie] for movie in common_films)
+    
+    ##sum up the squares
+    p1_sum_sq = sum((pow(USER_USING_OUR_WEBSITE_MOVIE_PREFERENCES[movie], 2) for movie in common_films))
+    p2_sum_sq = sum((pow(ROW_OF_DATABASE_THAT_MATCHES_USER[movie], 2) for movie in common_films))
+    
+    ##sum up products
+    product_sum = sum(USER_USING_OUR_WEBSITE_MOVIE_PREFERENCES[movie] * ROW_OF_DATABASE_THAT_MATCHES_USER[movie] for movie in common_films)
+    
+    ##get the pearson correlation
+    top = product_sum - ((p1_sum * p2_sum) / len(common_films))
+    bottom = sqrt((p1_sum_sq - pow(p1_sum, 2) / len(common_films)) * (p2_sum_sq - pow(p2_sum, 2) / len(common_films)))
+    
+    return top / bottom
+    
 def s_users(p1, database_ppl):
     ##TO DO: essentially, get all of the top scores (add a cutoff)
     ##store in dictionary/list? have the most similar "on top"
