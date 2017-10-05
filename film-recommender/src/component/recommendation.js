d73b7123f571660a4783db098f8144c79e0b2794
Liang Chen <lngchn85@gmail.com>
2017-10-05 19:18:09 -0400

    Change style and add remove button to seed film

--- film-recommender/src/component/recommendation.js
+++ film-recommender/src/component/recommendation.js
@@ -2,36 +2,72 @@ import React from 'react';
 import './recommendation.css';
 import movie01 from '../img/01.jpg';
 
+function SideBarFilter(props) {
+  return(
+    <nav className="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar text-left pt-3 border border-dark border-top-0 border-bottom-0 border-left-0">
+      <h4 className="ml-3 mb-1 text-light text-uppercase">Genre</h4>
+      <nav className="nav flex-column filter-link">
+        <a className="nav-item nav-link" href="#">Action & Adventure</a>
+        <a className="nav-item nav-link" href="#">Animation & Anime</a>
+        <a className="nav-item nav-link" href="#">Children's & Family</a>
+        <a className="nav-item nav-link" href="#">Comedy</a>
+        <a className="nav-item nav-link" href="#">Documentary</a>
+        <a className="nav-item nav-link" href="#">Drama</a>
+        <a className="nav-item nav-link" href="#">Horror</a>
+        <a className="nav-item nav-link" href="#">Sci-Fi & Fantasy</a>
+        <a className="nav-item nav-link text-right text-secondary filterLinkMore" href="#">More</a>
+      </nav>
+      <h4 className="mt-4 ml-3 mb-1 text-light text-uppercase">Rating</h4>
+      <nav className="nav flex-column filter-link">
+        <a className="nav-item nav-link" href="#">G</a>
+        <a className="nav-item nav-link" href="#">PG</a>
+        <a className="nav-item nav-link" href="#">PG-13</a>
+        <a className="nav-item nav-link" href="#">R</a>
+        <a className="nav-item nav-link" href="#">NR</a>
+        <a className="nav-item nav-link" href="#">NC-17</a>
+        <a className="nav-item nav-link text-right text-secondary filterLinkMore" href="#">More</a>
+      </nav>
+    </nav>
+  );
+}
+
+function MovieImage(props) {
+  return(
+    <div className="col-6 col-md-3 thumbnail">
+      <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
+      <h4 className="movie-title">The Martian</h4>
+      <button type="button" className="btn btn-outline-danger remove-button">-</button>
+    </div>
+  );
+}
+
 class Recommendation extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      seedMovies: [],
+      recommendationMovies: []
+    }  
+  }
+
+  componentDidMount() {
+    // Movie data is to be fetched here, and then seedMovies and recommendationMovies would
+    // map over the json instead of temp. 
+    const temp = [1, 2, 3, 4];
+    const seedMovies = temp.map(movie => <MovieImage data={movie} key={movie} />);
+    const recommendationMovies = temp.map(movie => <MovieImage data={movie} key={movie} />);
+
+    this.setState({
+      seedMovies,
+      recommendationMovies
+    });
+  }
+
   render() {
     return(
       <div className="container-fluid">
         <div className="row">
-          {/* Sidebar */}
-          <nav className="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar text-left pt-3 border border-dark border-top-0 border-bottom-0 border-left-0">
-            <h4 className="ml-3 mb-1 text-light text-uppercase">Genre</h4>
-            <nav className="nav flex-column filter-link">
-              <a className="nav-item nav-link" href="#">Action & Adventure</a>
-              <a className="nav-item nav-link" href="#">Animation & Anime</a>
-              <a className="nav-item nav-link" href="#">Children's & Family</a>
-              <a className="nav-item nav-link" href="#">Comedy</a>
-              <a className="nav-item nav-link" href="#">Documentary</a>
-              <a className="nav-item nav-link" href="#">Drama</a>
-              <a className="nav-item nav-link" href="#">Horror</a>
-              <a className="nav-item nav-link" href="#">Sci-Fi & Fantasy</a>
-              <a className="nav-item nav-link text-right text-secondary filterLinkMore" href="#">More</a>
-            </nav>
-            <h4 className="mt-4 ml-3 mb-1 text-light text-uppercase">Rating</h4>
-            <nav className="nav flex-column filter-link">
-              <a className="nav-item nav-link" href="#">G</a>
-              <a className="nav-item nav-link" href="#">PG</a>
-              <a className="nav-item nav-link" href="#">PG-13</a>
-              <a className="nav-item nav-link" href="#">R</a>
-              <a className="nav-item nav-link" href="#">NR</a>
-              <a className="nav-item nav-link" href="#">NC-17</a>
-              <a className="nav-item nav-link text-right text-secondary filterLinkMore" href="#">More</a>
-            </nav>
-          </nav>
+          <SideBarFilter />
 
           {/* Body */}
           <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pb-5 text-light recommendation">
@@ -40,54 +76,16 @@ class Recommendation extends React.Component {
               <button type="button" className="btn btn-outline-info ml-2" data-toggle="modal" data-target="#addSeedFilm" >+</button>
             </h2>
             <section className="row">
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
+              {this.state.seedMovies}
             </section>
             {/* Recommendations */}
             <h2 className="text-left mt-5">Recommendations</h2>
             <section className="row text-center placeholders">
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
-              <div className="col-6 col-md-3 thumbnail">
-                <img src={movie01} width="202" height="300" className="img-fluid movie-poster" alt="Movie Poster" />
-                <h4 className="movie-title">The Martian</h4>
-                <p className="movie-description lead">An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.</p> 
-              </div>
+              {this.state.recommendationMovies}
             </section>
           </main>
 
-          {/* Modal for Add a Seed Film */}
+          {/* Modal for Add a Seed Film Button */}
           <div className="modal fade" id="addSeedFilm" tabIndex="-1" role="dialog" aria-labelledby="addSeedFilmLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
               <div className="modal-content">
