c935bb4f9cabf6488d363bd7597a17bfa3bcc5fe
Liang Chen <lngchn85@gmail.com>
2017-10-10 19:50:14 -0400

    Add code to server.js to test MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient, assert = require('assert');
+const url = 'mongodb://localhost:27017/filmpro';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to MongoDB server");
+  db.close();
+});
+
 // Serve production build on Express server as static file
 // You can check the project at localhost:3001 (Express server, not React server)
 app.use(express.static(`${__dirname}/client/build`));
