832a5a122dbce4eef225cb782eb1ffa88d5ed9d2
Dan Cambitsis <dcam90@gmail.com>
2017-10-09 22:37:59 -0400

    Update collab1.py

--- python-script/collab1.py
+++ python-script/collab1.py
@@ -1,4 +1,4 @@
-####################################### 10/9/17 8:49 PM
+####################################### 10/9/17 10:38 PM
 
 import json
 import glob
@@ -35,8 +35,11 @@ def do_append(the_dict, the_info):
     for info in the_info:
         str_info = str(info)
         split_info = [word.strip(string.punctuation) for word in str_info.split("'")]
-        if len(split_info[14]) > 2: the_dict[split_info[14] + split_info[15]] = int(split_info[3])
-        else: the_dict[split_info[15]] = int(split_info[3])
+        if split_info[3] == '': continue ##ignore empty ratings
+        if len(split_info[14]) > 2:
+            combine = split_info[14] + split_info[15]
+            the_dict[combine] = int(split_info[3]) ## *** FIX THIS ISSUE *** 
+        the_dict[split_info[15]] = int(split_info[3])
 
 #get all of the json files and put it into a list, first user of the list is me (for now)
 def get_json_files(store):
