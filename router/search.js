d6b4912d6c42776655cbd7be0a366066b8db1381
Liang Chen <liang.r.chen@gmail.com>
2017-11-02 01:25:43 -0400

    Add Express API to create and retrieve user; Add node.js dependencies for authentication

--- router/search.js
+++ router/search.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
-const bodyParser = require('body-parser');
 var request = require('request');
 
 const TMDB_KEY = require('../tmdb-api-key');
 const API_KEY = TMDB_KEY.key;
-
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/', (req, res) => {
   let query = req.body.query;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve production build on Express server as static file
 // You can check the project at localhost:3001 (Express server, not React server)
@@ -8,10 +12,12 @@ app.use(express.static(`${__dirname}/client/build`));
 // Use the film router
 // Usage example: localhost:3001/film
 // In React, you can call fetch to /film
-var film = require('./router/film');
-var search = require('./router/search');
+const film = require('./router/film');
+const search = require('./router/search');
+const user = require('./router/user');
 app.use('/film', film);
 app.use('/search', search);
+app.use('/user', user);
 
 app.get('/hello', (req, res) => {
   res.json({message: "Hello World"});
