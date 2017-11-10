b075faab2b9400d5aa6ce723c205dc57cd7cf908
Kwabena Appiah <appiah1@gmail.com>
2017-11-10 01:41:52 -0500

    Update index.js
    Added the line "router.use('/', require('./call_film-based_cf'))".

--- controller/index.js
+++ controller/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.use('/', require('./user'));
 router.use('/film', require('./film'));
 router.use('/search', require('./search'));
+router.use('/user', require('./user'));
+router.use('/', require('./call_film-based_cf'));
 
 module.exports = router;
