3adf65531447713be9302c21164f759d80076f9e
Liang Chen <liang.r.chen@gmail.com>
2017-11-10 12:18:32 -0500

    Remove unsed express router

--- controller/index.js
+++ controller/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use('/', require('./user'));
 router.use('/film', require('./film'));
 router.use('/search', require('./search'));
-router.use('/user', require('./user'));
+
 router.use('/call_film-based_cf', require('./call_film-based_cf'));
 
 module.exports = router;
