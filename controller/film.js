394b37503f113adbb0309f54258501c668bb2420
Liang Chen <liang.r.chen@gmail.com>
2017-11-15 02:11:28 -0500

    Create film API for movie detail page

--- controller/film.js
+++ controller/film.js
@@ -1,60 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const url = process.env.MongoDB_URL;
+const axios = require('axios');
 
-function insertFilms(db, callback) {
-  // Get the films collection
-  var collection = db.collection('films');
-  // Insert some films
-  collection.insertMany([
-    {film_id: 1},
-    {film_id: 2},
-    {film_id: 3}
-  ], (err, result) => {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 films into the collection");
-    callback(result);
-  });
-}
-
-function findFilms(db, callback) {
-  // Get the films collection
-  var collection = db.collection('films');
-  // Find all films
-  collection.find({}).toArray((err, docs) => {
-    assert.equal(err, null);
-    callback(docs);
-  });
-}
-
-router.post('/', (req, res) => {
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    insertFilms(db, () => {
-      db.close();
-    });
-  });
-
-  res.sendStatus(200);  // 'OK' status
-});
-
-router.get('/', (req, res) => {
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    findFilms(db, (result) => {
-      db.close();
-      res.json(result);
-    });
-  });
-});
+const API_KEY = process.env.TMDB_KEY;
 
 router.get('/:id', (req, res) => {
-  res.json({message: 'Message from /film/:id'});
+  let id = req.params.id;
+
+  axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=credits,keywords,videos`)
+    .then(result => res.json(result.data))
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(400);
+    });
 });
 
 module.exports = router;
