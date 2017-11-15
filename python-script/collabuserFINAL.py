6713fd175353cb63b18638ad4bac01e42e56bbbd
Dan Cambitsis <dcam90@gmail.com>
2017-11-15 12:03:42 -0500

    Update collabuserFINAL.py

--- python-script/collabuserFINAL.py
+++ python-script/collabuserFINAL.py
@@ -1,4 +1,4 @@
-####################################### 11/12/17 12:42 PM
+####################################### 11/15/17 12:03 PM
 
 import json
 import glob
@@ -160,21 +160,11 @@ def rec_movies(me, sim_score, movie_id_store, all_movies):
 
     return rankings
 
-#Output each imdb id one by one.
-#The Node.js that called this file will store each id and return it to the server.
-#For the purposes of this example, each id will only be printed to the terminal. 
-def output_data(films_to_rec, movie_id_store):
-    for i in films_to_rec:
-      print movie_id_store[films_to_rec[0]]
-    
 #read in user_obj data
 def read_in():
-    user_obj = json.load(sys.stdin) 
+    user_obj = sys.stdin.readlines()
 
-    #Uncomment the following line to test locally without the a node.js file
-    #user_obj = {"films":[{"id":297762,"imdb_id":"tt0451279","title":"Wonder Woman","poster_path":"/imekS7f1OuHyUP2LAiTEM0zBzUz.jpg"},{"id":346364,"imdb_id":"tt1396484","title":"It","poster_path":"/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg"},{"id":13640,"imdb_id":"tt0934706","title":"Superman: Doomsday","poster_path":"/3of4nShmv1hBmrebOQqGlfZ9ZL0.jpg"},{"id":1726,"imdb_id":"tt0371746","title":"Iron Man","poster_path":"/848chlIWVT41VtAAgyh9bWymAYb.jpg"},{"id":272,"imdb_id":"tt0372784","title":"Batman Begins","poster_path":"/dr6x4GyyegBWtinPBzipY02J2lV.jpg"},{"id":34127,"imdb_id":"tt0038260","title":"Wonder Man","poster_path":"/uvvdbVmk04jNFH0Zl3qsgAIsAXa.jpg"},{"id":315635,"imdb_id":"tt2250912","title":"Spider-Man: Homecoming","poster_path":"/kY2c7wKgOfQjvbqe7yVzLTYkxJO.jpg"},{"id":588,"imdb_id":"tt0384537","title":"Silent Hill","poster_path":"/4Zz9cF8S4E7DITosNYh3spybYJb.jpg"},{"id":1576,"imdb_id":"tt0120804","title":"Resident Evil","poster_path":"/5jdvEi57WBGuI5n2drG8FLAbYDp.jpg"},{"id":293660,"imdb_id":"tt1431045","title":"Deadpool","poster_path":"/inVq3FRqcYIRl2la8iZikYYxFNR.jpg"}], "user_id":"5a024f9b60375008f87cfe07" }
-    
-    return user_obj
+    return json.loads(user_obj[0])
 
 #########
       
@@ -183,10 +173,14 @@ def main():
     movie_id_store = {}
     sim_score = {}
     all_movies = {} #used for remove_x
+    output_str = ""
  
     films_to_rec = rec_movies(user_obj["films"], sim_score, movie_id_store, all_movies)
     
-    output_data(films_to_rec, movie_id_store)
+    for x in films_to_rec:
+        output_str += movie_id_store[x[0]] + " "
+
+    print output_str
         
 if __name__ == "__main__":
     main()
