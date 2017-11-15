e1b8d6f6779b0f4737ccbb61161d0a9f962cdf8b
Kwabena Appiah <appiah1@gmail.com>
2017-11-15 02:02:37 -0500

    Update receive_json_obj.py
    Each film ID is now concatenated together (with a space in between) and then printed out into a single string.

--- python-script/receive_json_obj.py
+++ python-script/receive_json_obj.py
@@ -1,36 +1,26 @@
-#11-12-17, 9:27 pm
-#Test script for receiving a JSON obbject.
-#Receives data from the Node.js script 'send_json_obj.js'.
-#The code here will be incorporated into our collabrative filtering Python scripts.  
-
+#11-15-17, 2:02 am
 import sys, json
 
-#read in data from Node.js
+#Read in data from Node.js
 def read_in():
     lines = sys.stdin.readlines()
     return json.loads(lines[0])
 
-#init and manipulate the data within the imported JSON object.
 def init():
-    user_obj = read_in()    
-    user_id = user_obj["user_id"]
-    imdb_dict = {} 
- 
+    user_obj = read_in()  
+    #user_id = user_obj["user_id"]
+    output_str =''
+    
+    #Concatenate each film id with a space in between 
     for i in range(0, len(user_obj["films"])):
       x = user_obj["films"][i]
-      imdb_dict[str(x["id"])] = x["imdb_id"]
-      print(imdb_dict)
+      output_str +=x["imdb_id"] + " "
 
-#Output each imdb id one by one.
-#The Node.js that called this file will store each id and return it to the server.
-#For the purposes of this example, each id will only be printed to the terminal. 
-# def output_data(imdb_dict):
-#     for i in imdb_dict:
-#       print (imdb_dict[i])
-    
+    #Exit the loop and print out a single concatenated string of film IDs
+    print(output_str)
+
 def main():
     init()
    
-
 if __name__ == '__main__':
     main()
