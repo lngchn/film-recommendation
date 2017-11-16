d4b9036e19c186b8aeff2e7bd45d433480abf9f3
Liang Chen <liang.r.chen@gmail.com>
2017-11-16 17:03:39 -0500

    Adjust text in Home page

--- client/src/component/home.js
+++ client/src/component/home.js
@@ -6,12 +6,11 @@ class Home extends Component {
     return(
       <div className="jumbotron jumbotron-fluid m-0" id="home-background">
         <div className="container text-light text-center" id="home-content">
-          <h1 className="display-3">WELCOME TO FILM PRO!</h1>
-          <p className="lead text-left">Film Pro is the perfect guide for your adventures in
-movie watching. <br />If you're feeling adventurous, click below to begin your journey into cinema! 
-      </p>
+          <h1 className="display-3 text-uppercase text-center">Weclome to Film Pro!</h1>
+          <p className="lead text-center">Film Pro is the perfect guide for your adventures in movie watching. <br />If you're feeling adventurous, click below to begin your journey into cinema!</p>
           <p className="lead">
-            <a className="btn btn-light btn-lg" href="/register" role="button">Get Started</a>
+            <a className="btn btn-light btn-lg mr-2" href="/register" role="button">Sign Up</a>
+            <a className="btn btn-light btn-lg ml-2" href="/tutorial" role="button">Tutorial</a>
           </p>
         </div>
       </div>
