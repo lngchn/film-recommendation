f79945d518d22b7771b48d0f83d736471b3f00f5
Kwabena Appiah <appiah1@gmail.com>
2017-11-16 14:48:09 -0500

    Update home.js
    Updated text.

--- client/src/component/home.js
+++ client/src/component/home.js
@@ -6,9 +6,9 @@ class Home extends Component {
     return(
       <div className="jumbotron jumbotron-fluid m-0" id="home-background">
         <div className="container text-light text-center" id="home-content">
-          <h1 className="display-3">Welcome to Film Pro!</h1>
+          <h1 className="display-3">WELCOME TO FILM PRO!</h1>
           <p className="lead text-left">Film Pro is the perfect guide for your adventures in
-movie watching. If you're feeling adventurous, click below to begin your journey into cinema! 
+movie watching. <br />If you're feeling adventurous, click below to begin your journey into cinema! 
       </p>
           <p className="lead">
             <a className="btn btn-light btn-lg" href="/register" role="button">Get Started</a>
