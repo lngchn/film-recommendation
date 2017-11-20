a3a3c24320427db516b837848ff791b9ba37d840
Kwabena Appiah <appiah1@gmail.com>
2017-11-19 20:06:03 -0500

    Add files via upload
    Created a placeholder file for "movie.js".

---
+++ client/src/component/movie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import './movie.css';
+
+import { Redirect } from 'react-router-dom';
+
+class Movie extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return(<p>Placeholder</p>);
+  }
+}
+
+export default Movie;
