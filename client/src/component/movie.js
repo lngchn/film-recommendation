79e56b1cb0e6d7026d6355c5e991176b58e0cc5d
Liang Chen <liang.r.chen@gmail.com>
2017-11-23 00:33:19 -0500

    Add placeholder for movie.css

--- client/src/component/movie.js
+++ client/src/component/movie.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-//import './movie.css';
-
-import { Redirect } from 'react-router-dom';
+import './movie.css';
 
 class Movie extends Component {
-  constructor() {
-    super();
-
-  }
-
   render() {
     return(<p>Placeholder</p>);
   }
