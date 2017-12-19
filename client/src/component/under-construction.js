aea61b7963bfe5d72950a8b0c61019c2987a5979
Kwabena Appiah <appiah1@gmail.com>
2017-12-19 00:21:28 -0500

    Update under-construction.js
    Added redirect functionality.

--- client/src/component/under-construction.js
+++ client/src/component/under-construction.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './under-construction.css';
 
+import { Redirect } from 'react-router-dom';
+
 class UnderConstruction extends Component {
+  constructor() {
+    super();
+	this.state = {
+      isRedirect: false
+    }
+  	this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  handleRedirect(event) {
+  	this.setState({isRedirect: true});
+  }
   render() {
+  	let isRedirect = this.state.isRedirect;
+
+    if(isRedirect) {
+      return(<Redirect to="/" />);
+    }
     return(
       <div className="jumbotron jumbotron-fluid m-0 text-center" id="under-construction">
         <div className="container" id="under-construction-content">
           <h1 className="display-4"><i className="fa fa-hourglass-o fa-lg" aria-hidden="true"></i> Page Under Construction</h1>
-          <button type="button" className="btn btn-outline-success mt-4 p-3">Take Me To The Homepage</button>
+          <button type="button" className="btn btn-outline-success mt-4 p-3" onClick={this.handleRedirect}>Take me to the homepage</button>
         </div>
       </div>
     );
