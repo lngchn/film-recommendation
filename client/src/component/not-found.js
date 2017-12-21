6a2a48bdd73afb02eeabf90eb9856e1f8cfb84d4
Liang Chen <liang.r.chen@gmail.com>
2017-12-21 01:09:28 -0500

    Add redirect to homepage

--- client/src/component/not-found.js
+++ client/src/component/not-found.js
@@ -2,33 +2,13 @@ import React, { Component } from 'react';
 import './not-found.css';
 
 class NotFound extends Component {
-  constructor() {
-    super();
-    this.state = {
-      message: ''
-    }
-  }
-
-  componentDidMount() {
-    return fetch('/hello')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          message: json.message
-        });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  }
-
   render() {
     return(
       <div className="jumbotron jumbotron-fluid m-0 text-center" id="not-found">
         <div className="container" id="not-found-content">
           <h1 className="display-4"><i className="fa fa-frown-o fa-lg" aria-hidden="true"></i> 404 Not Found</h1>
           <p className="lead">We couldn't find what you're looking for.</p>
-          <button type="button" className="btn btn-outline-success mt-2 p-3">Take Me To The Homepage</button>
+          <a href="/"><button type="button" className="btn btn-outline-success mt-2 p-3">Take Me To The Homepage</button></a>
         </div>
       </div>
     );
--- client/src/component/under-construction.js
+++ client/src/component/under-construction.js
@@ -7,7 +7,7 @@ class UnderConstruction extends Component {
       <div className="jumbotron jumbotron-fluid m-0 text-center" id="under-construction">
         <div className="container" id="under-construction-content">
           <h1 className="display-4"><i className="fa fa-hourglass-o fa-lg" aria-hidden="true"></i> Page Under Construction</h1>
-          <button type="button" className="btn btn-outline-success mt-4 p-3">Take Me To The Homepage</button>
+          <a href="/"><button type="button" className="btn btn-outline-success mt-4 p-3">Take Me To The Homepage</button></a>
         </div>
       </div>
     );
